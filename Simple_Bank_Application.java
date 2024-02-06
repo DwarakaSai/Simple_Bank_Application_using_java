a38adb45f10639b7da2b819b25aa66830e9187f8
DwarakaSai <61784243+DwarakaSai@users.noreply.github.com>
2024-02-06 13:54:01 +0530

    Create Simple_Bank_Application.java

---
+++ Simple_Bank_Application.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class Simple_Bank_Application {
+  static double balance = 0;
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int opt = 0;
+    while (opt != 4) {
+      System.out.println("Welcome to the Bank");
+      System.out.println("1. Check Balance");
+      System.out.println("2. Deposit");
+      System.out.println("3. Withdraw");
+      System.out.println("4. Exit");
+      System.out.print("Enter an option: ");
+      opt = scanner.nextInt();
+
+      switch (opt) {
+        case 1:
+          checkBalance();
+          break;
+        case 2:
+          deposit();
+          break;
+        case 3:
+          withdraw();
+          break;
+        case 4:
+          exit();
+          break;
+        default:
+          System.out.println("Invalid option. Try again. Please choose options from 1 to 4.");
+          break;
+      }
+    }
+  }
+
+  public static void checkBalance() {
+    System.out.println("Your current balance is Rs." + balance);
+  }
+
+  public static void deposit() {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter the amount to deposit: Rs.");
+    double amount = scanner.nextDouble();
+    balance += amount;
+    System.out.println("Rs." + amount + " has been deposited to your account.");
+    checkBalance();
+  }
+
+  public static void withdraw() {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter the amount to withdraw: Rs.");
+    double amount = scanner.nextDouble();
+    if (amount > balance) {
+      System.out.println("Insufficient funds.");
+    } else {
+      balance -= amount;
+      System.out.println("Rs." + amount + " has been withdrawn from your account.");
+    }
+    checkBalance();
+  }
+
+  public static void exit() {
+    System.out.println("Thank you for banking with us. Have a great day!");
+  }
+}
